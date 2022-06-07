2b33b8fe5cbe21ac452fde89238e65a03c50306f
Your Name <you@example.com>
2022-06-07 10:06:30 +0300

    изменена ошиюка с D

--- RSA 2/RSA 2.cpp
+++ RSA 2/RSA 2.cpp
@@ -2,11 +2,41 @@
 #include <math.h>
 #include <string>
 using namespace std;
+int long Calculate_d(long m)
+{
+	long d = m-1 ;
+
+	for (long i = 2; i <= m; i++)
+		if ((m % i == 0) && (d % i == 0)) //если имеют общие делители
+		{
+			d--;
+			i = 1;
+		}
+
+	return d-1;
+}
+
+int long Calculate_e(long d, long m)
+{
+	long e = 10;
+
+	while (true)
+	{
+		if ((e * d) % m == 1)
+			break;
+		else
+			e++;
+	}
+
+	return e;
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	 long long p, q, n, e, f, c, m, d, pick,m1,c1;
-	 long long d1;
+	 long long p, q, n, e, f, c, m,  pick,m1,c1;
+	 long long d = 0;
+	 
 	cout << "Выберите что вы хотите сделать:" << endl;
 	cout << "1)Зашифровать сообщение" << endl;
 	cout << "2)Расшифровать сообщение" << endl;
@@ -22,8 +52,7 @@ int main()
 		f = (p - 1) * (q - 1);
 		e = f - 1;
 		cout << "Открытый ключ= {" << e << " " << n << "}" << endl;
-		d1 = 1/e ;
-		d = d1 % f;
+		d = Calculate_d(m);
 		cout << "НИКОМУ НЕ ПОКАЗЫВАЙТЕ ЗАКРЫТЫЙ КЛЮЧ" << endl;
 		cout << "Закрытый ключ= {" << d << " " << n << "}" << endl;
 		c1 = pow(m, e) ;
@@ -37,12 +66,12 @@ int main()
 		cin >> p;
 		cout << "c=" << endl;
 		cin >> c;
+		cout << "d=";
+		cin >> d;
 		n = p * q;
 		f = (p - 1) * (q - 1);
 		e = f - 1;
 		cout << "Открытый ключ= {" << e << " " << n << "}" << endl;
-		d1 = pow(e, -1) ;
-		d = d1 % f;
 		cout << "НИКОМУ НЕ ПОКАЗЫВАЙТЕ ЗАКРЫТЫЙ КЛЮЧ" << endl;
 		cout << "Закрытый ключ= {" << d << " " << n << "}" << endl;
 		m1 = pow(c, d);
